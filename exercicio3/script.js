db13a98fcbaffe4f6130a4e71c5e6f2ae7cc8c25
Luana Trevizani <luanaftrevizani@gmail.com>
2022-09-08 19:03:58 -0300

    exercicios corrigidos

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,23 @@
 //código a ser reescrito
+const nome = prompt('Digite seu nome:').toLowerCase()
+const idade = Number(prompt('Digite sua idade:'))
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+nome === 'jose' ? console.log('Oi, Zé!') : console.log('Olá ' + nome)
+idade >= 18
+  ? console.log('Pode tirar carteira de motorista!')
+  : console.log('Ainda não pode tirar carteira de motorista.')
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// # Exercício 3
+
+// Reescreva os códigos do arquivo script.js utilizando o if ternário.
+
+// **Relembrando o if ternário**
+// ```jsx
+// condição ? expr1 : expr2
+// ```
+
+// Onde :
+
+// `condição` é uma expressão que é avaliada como `true` ou `false`.
+
+// `expr1`, `expr2` são xpressões com valores de qualquer tipo.
